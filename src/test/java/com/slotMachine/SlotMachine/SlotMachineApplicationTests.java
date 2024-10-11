553bce0272e4a608193a71f3557e165b16038ad3
mohannada <mohannad@wnwd.com>
2024-10-11 22:37:38 +0300

    first commit base project

---
+++ src/test/java/com/slotMachine/SlotMachine/SlotMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slotMachine.SlotMachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlotMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
