0d76ce0c06f660be2641d637d98110012bc2eb9a
mohannada <mohannad@wnwd.com>
2024-10-11 22:37:38 +0300

    first commit base project

---
+++ src/main/java/com/slotMachine/SlotMachine/SlotMachineApplication.java
@@ -0,0 +1,13 @@
+package com.slotMachine.SlotMachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlotMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlotMachineApplication.class, args);
+	}
+
+}
