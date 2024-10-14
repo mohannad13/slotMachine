6e5b3385b1c7eb194de175e73d5fe611c56bc6d4
mohannada <mohannad@wnwd.com>
2024-10-14 13:10:11 +0300

    add basic tests

--- src/main/java/com/slotMachine/SlotMachine/services/SlotMachineService.java
+++ src/main/java/com/slotMachine/SlotMachine/services/SlotMachineService.java
@@ -25,7 +25,6 @@ public class SlotMachineService {
             }
 
             missionService.checkAndApplyRewards(player);
-
             return result;
         } else {
             throw new IllegalArgumentException("No spins left for the player!");
